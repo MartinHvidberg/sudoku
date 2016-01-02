2a14116ee879acb8c251ab8c6fdbf2e0eb67e330
MartinHvidberg <MartinHvidberg@localhost>
2016-01-02 15:14:27 +0100

    1601011515 More Re-Class

--- src/SuDoKu.py
+++ src/SuDoKu.py
@@ -43,7 +43,6 @@ class SuDoKu(object):
         """ initialises a SuDoKu object """
         self.m = [['0' for i in range(9)] for j in range(9)] # Empty (0 filled) matrix
         self.p = [[set() for i in range(9)] for j in range(9)] # Empty (empty lists) pencil-matrix
-        self.rec = list() # Track record of solving tactics steps
         if len(str_ini) == 0:
             pass # Creating empty sudoku ...
         elif len(str_ini) == 81:
@@ -356,7 +355,103 @@ class SuDoKu(object):
                 raise CustomException(" stats() No track record found - this is really strange :-(")
         else:
             raise CustomException(" stats() takes a valid SuDoKu. This is Not valid: "+str(self.m))
-                
+            
+    #====== Printout functions ======
+    
+    def __str__(self):
+        return self.show_current()
+    
+    def show_small(self, matrix):
+        def p(i,j):
+            return str(matrix[i][j])
+        def l(i):
+            return ("|"+p(i,0)+p(i,1)+p(i,2)+"|"+p(i,3)+p(i,4)+p(i,5)+"|"+p(i,6)+p(i,7)+p(i,8)+"|\n").replace('0',' ')
+        def q():
+            v = '+---+---+---+\n'
+            return (v+l(0)+l(1)+l(2)+v+l(3)+l(4)+l(5)+v+l(6)+l(7)+l(8)+v).strip()
+        return q()
+    
+    def pencils_as_lol(self):
+        return [[list(self.p[i][j]) for j in range(9)] for i in range(9)]
+    
+    def pencils_as_string(self):
+        lst_ret = self.pencils_as_lol()
+        str_ret = "("
+        for lst_r in lst_ret:
+            for lst_p in lst_r:
+                lst_p.sort()
+                str_ret += str(lst_p).replace('[','').replace(']','').replace(',','').replace(' ','')+','
+            str_ret += ';'
+        return str_ret.strip(';')+')'
+    
+    def show_current(self):
+        return self.show_small(self.m)
+    
+    def show_solved(self):
+        if self.v:
+            return self.show_small(self.solution)
+        else:
+            return "SuDoKu can't be solved, since it's invalid..."
+    
+    def show_big(self):
+        lines = [['0' for i in range(37)] for j in range(37)]
+        # Build graticules
+        for p in range(37):
+            if p in (0,12,24,36):
+                lines[p] = "+===========+===========+===========+"
+            elif p in (4,8,16,20,28,32,36):
+                lines[p] = "|---+---+---|---+---+---|---+---+---|"
+            else:
+                lines[p] = "|   !   !   |   !   !   |   !   !   |"
+        # Fill values # XXX This can be cleaned up - cci and ccj pre-calcs
+        for i in range(9):
+            for j in range(9):
+                cci = i*4+2
+                ccj = j*4+2
+                if self.m[i][j] != 0: # show solved value
+                    lines[cci-1] = lines[cci-1][:ccj-1] +'...'+ lines[cci-1][ccj+2:]
+                    lines[cci] = lines[cci][:ccj-1] +'.'+str(self.m[i][j])+'.'+ lines[cci][ccj+2:]
+                    lines[cci+1] = lines[cci+1][:ccj-1] +'...'+ lines[cci+1][ccj+2:]
+                else: # show pencil marks
+                    if 1 in self.p[i][j]:
+                        lines[cci-1] = lines[cci-1][:ccj-1] +str(1)+ lines[cci-1][ccj:]
+                    if 2 in self.p[i][j]:
+                        lines[cci-1] = lines[cci-1][:ccj] +str(2)+ lines[cci-1][ccj+1:]
+                    if 3 in self.p[i][j]:
+                        lines[cci-1] = lines[cci-1][:ccj+1] +str(3)+ lines[cci-1][ccj+2:]
+                    if 4 in self.p[i][j]:
+                        lines[cci] = lines[cci][:ccj-1] +str(4)+ lines[cci][ccj:]
+                    if 5 in self.p[i][j]:
+                        lines[cci] = lines[cci][:ccj] +str(5)+ lines[cci][ccj+1:]
+                    if 6 in self.p[i][j]:
+                        lines[cci] = lines[cci][:ccj+1] +str(6)+ lines[cci][ccj+2:]
+                    if 7 in self.p[i][j]:
+                        lines[cci+1] = lines[cci+1][:ccj-1] +str(7)+ lines[cci+1][ccj:]
+                    if 8 in self.p[i][j]:
+                        lines[cci+1] = lines[cci+1][:ccj] +str(8)+ lines[cci+1][ccj+1:]
+                    if 9 in self.p[i][j]:
+                        lines[cci+1] = lines[cci+1][:ccj+1] +str(9)+ lines[cci+1][ccj+2:]
+                        
+        # to strings
+        str_ret = ""
+        for line in lines:
+            for pin in line:
+                str_ret += str(pin)
+            str_ret += '\n'
+        return str_ret
+                        
+    def show_pencil(self):
+        return self.show_big()
+
+class Slap(SuDoKu):
+    
+    STPS = ['Free gifts', 'Crosshatching', 'Naked singles', 'Locked Cand.']
+    
+    def __init__(self, str_ini, lst_hardess = STPS):
+        SuDoKu.__init__(self, str_ini)
+        self.hardness = lst_hardess
+        self.rec = list() # Track record of solving tactics steps
+                        
     #====== SLAP (Solve Like A Person) solver functions ======
     
     def record(self, dic_hit):
@@ -453,101 +548,7 @@ class SuDoKu(object):
                             log.info('Seem to have exhausted all solving strategies ...')
                             return
         log.info('Seem to have solved the SuDoKu. Thanks for using SLAP :-)')
-            
-    #====== Printout functions ======
-    
-    def __str__(self):
-        return self.show_current()
-    
-    def show_small(self, matrix):
-        def p(i,j):
-            return str(matrix[i][j])
-        def l(i):
-            return ("|"+p(i,0)+p(i,1)+p(i,2)+"|"+p(i,3)+p(i,4)+p(i,5)+"|"+p(i,6)+p(i,7)+p(i,8)+"|\n").replace('0',' ')
-        def q():
-            v = '+---+---+---+\n'
-            return (v+l(0)+l(1)+l(2)+v+l(3)+l(4)+l(5)+v+l(6)+l(7)+l(8)+v).strip()
-        return q()
-    
-    def pencils_as_lol(self):
-        return [[list(self.p[i][j]) for j in range(9)] for i in range(9)]
-    
-    def pencils_as_string(self):
-        lst_ret = self.pencils_as_lol()
-        str_ret = "("
-        for lst_r in lst_ret:
-            for lst_p in lst_r:
-                lst_p.sort()
-                str_ret += str(lst_p).replace('[','').replace(']','').replace(',','').replace(' ','')+','
-            str_ret += ';'
-        return str_ret.strip(';')+')'
-    
-    def show_current(self):
-        return self.show_small(self.m)
-    
-    def show_solved(self):
-        if self.v:
-            return self.show_small(self.solution)
-        else:
-            return "SuDoKu can't be solved, since it's invalid..."
-    
-    def show_big(self):
-        lines = [['0' for i in range(37)] for j in range(37)]
-        # Build graticules
-        for p in range(37):
-            if p in (0,12,24,36):
-                lines[p] = "+===========+===========+===========+"
-            elif p in (4,8,16,20,28,32,36):
-                lines[p] = "|---+---+---|---+---+---|---+---+---|"
-            else:
-                lines[p] = "|   !   !   |   !   !   |   !   !   |"
-        # Fill values # XXX This can be cleaned up - cci and ccj pre-calcs
-        for i in range(9):
-            for j in range(9):
-                cci = i*4+2
-                ccj = j*4+2
-                if self.m[i][j] != 0: # show solved value
-                    lines[cci-1] = lines[cci-1][:ccj-1] +'...'+ lines[cci-1][ccj+2:]
-                    lines[cci] = lines[cci][:ccj-1] +'.'+str(self.m[i][j])+'.'+ lines[cci][ccj+2:]
-                    lines[cci+1] = lines[cci+1][:ccj-1] +'...'+ lines[cci+1][ccj+2:]
-                else: # show pencil marks
-                    if 1 in self.p[i][j]:
-                        lines[cci-1] = lines[cci-1][:ccj-1] +str(1)+ lines[cci-1][ccj:]
-                    if 2 in self.p[i][j]:
-                        lines[cci-1] = lines[cci-1][:ccj] +str(2)+ lines[cci-1][ccj+1:]
-                    if 3 in self.p[i][j]:
-                        lines[cci-1] = lines[cci-1][:ccj+1] +str(3)+ lines[cci-1][ccj+2:]
-                    if 4 in self.p[i][j]:
-                        lines[cci] = lines[cci][:ccj-1] +str(4)+ lines[cci][ccj:]
-                    if 5 in self.p[i][j]:
-                        lines[cci] = lines[cci][:ccj] +str(5)+ lines[cci][ccj+1:]
-                    if 6 in self.p[i][j]:
-                        lines[cci] = lines[cci][:ccj+1] +str(6)+ lines[cci][ccj+2:]
-                    if 7 in self.p[i][j]:
-                        lines[cci+1] = lines[cci+1][:ccj-1] +str(7)+ lines[cci+1][ccj:]
-                    if 8 in self.p[i][j]:
-                        lines[cci+1] = lines[cci+1][:ccj] +str(8)+ lines[cci+1][ccj+1:]
-                    if 9 in self.p[i][j]:
-                        lines[cci+1] = lines[cci+1][:ccj+1] +str(9)+ lines[cci+1][ccj+2:]
-                        
-        # to strings
-        str_ret = ""
-        for line in lines:
-            for pin in line:
-                str_ret += str(pin)
-            str_ret += '\n'
-        return str_ret
-                        
-    def show_pencil(self):
-        return self.show_big()
 
-class Slap(SuDoKu):
-    
-    STPS = ['Free gifts', 'Crosshatching', 'Naked singles', 'Locked Cand.']
-    
-    def __init__(self, str_ini, lst_hardess = STPS):
-        SuDoKu.__init__(self, str_ini)
-        self.hardness = lst_hardess
     
 class Slam(SuDoKu):
     

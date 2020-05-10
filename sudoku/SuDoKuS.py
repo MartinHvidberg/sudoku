f8bafad872e89e8f7743512b0bec3aa5dc2282fe
martin <tantdyalf.helix>
2020-05-10 17:23:18 +0200

    SuDoKuX - Very simple a() works like a charm ...

--- sudoku/SuDoKuS.py
+++ sudoku/SuDoKuS.py
@@ -6,7 +6,7 @@ Nomenclature:
 lst_sdk = The SoDuKo. List of 81 integers [0..9] where 0 means empty
 lst_pcm = Pencil-marks. List of 81 set of int [1..9]
 sdk     = The SuDoKu with pencil-marks and other meta-data. {s: lst_sdk, p: lst_pcm, ...}
-i_... functions operate directly on the 'inner' variables, e.g. lst_pcm
+_i functions operate directly on the 'inner' variables, e.g. lst_pcm
 n: Number of the cell in a 81 count of cells [0..80]
 i: number of col (all cells on same vertical line) [0..8]
 j: number of row (all cells on same horizontal line) [0..8]
@@ -28,11 +28,11 @@ def same_col(n,m): return (n-m) % 9 == 0
 
 def same_box(n,m): return (n//27 == m//27 and n%9//3 == m%9//3)
 
-def n2ij(n):
-    return
-
-def ij2n(i, j):
-    return
+# def n2ij(n):
+#     return
+#
+# def ij2n(i, j):
+#     return
 
 def str2loi(str_in):
     """ Convert a string form of a SuDoKu to a list of integers
@@ -61,19 +61,19 @@ def loi2sdk(loi_in):
     """ Make a sdk (dic of lst_sdk and lst_pcm) from a loi_sdk """
     dic_ret = dict()
     dic_ret['s'] = loi_in  # The actual SuDoKu (81x integers)
-    dic_ret['p'] = i_make_pcm(loi_in)  # Pencil marks
+    dic_ret['p'] = pncl_make_i(loi_in)  # Pencil marks
     dic_ret['sol'] = set()  # Set of solutions found, so far
     dic_ret['bum'] = False  # True if we hit a dead end
     return dic_ret
 
-def valid_sdk(obj):
+def is_valid(obj):
     if not isinstance(obj, dict):
         return False
     if not all([k in obj.keys() for k in ['s', 'p', 'sol', 'bum']]):
         return False
     return True
 
-def i_show_small(lst_sdk):
+def show_small_i(lst_sdk):
     """ Show the SoDuKo in 2D
     Return a string version for print, with a minimalistic design, of the SoDuKo in the lst_sdk
     :param lst_sdk: the input SoDuKo as a standard list of integers, length = 81
@@ -89,35 +89,45 @@ def i_show_small(lst_sdk):
     return q()
 
 def show_small(sdk_l):
-    if not valid_sdk(sdk_l) : raise RuntimeError("Not a valid sdk in: show_small()")
-    return i_show_small(sdk_l['s'])
+    if not is_valid(sdk_l) : raise RuntimeError("Not a valid sdk in: show_small()")
+    return show_small_i(sdk_l['s'])
 
-def set_cell(n, v, lst_sdk_l):
+def set_cell_i(n, v, lst_sdk_l):
     """ Set cell number n to value v """
     lst_sdk_l[n] = v
     return lst_sdk_l
 
-def upd_pncl(num_cell, num_valu, lst_pnc_l):
-    """ Update all pencil-marks, as if num_cell is given num_valu
-    return updated list """
-    for m in range(81):  # update pencil-marks
-        if same_col(m, num_cell) or same_row(m, num_cell) or same_box(m, num_cell):
-            lst_pnc_l[m].discard(num_valu)
-    return lst_pnc_l
+def set_cell(n, v, sdk_l):
+    sdk_l['s'] = set_cell_i(n, v, sdk_l['s'])
+    return sdk_l
 
 def place(num_valu, num_cell, sdk_l):
-    """ Place value num_valu in cell num_cell, and update all pencil-marks """
-    if not valid_sdk(sdk_l) : raise RuntimeError("Not a valid sdk in: place()")
-    sdk_l['s'] = set_cell(num_cell, num_valu, sdk_l['s'])
-    sdk_l['p'] = upd_pncl(num_cell, num_valu, sdk_l['p'])
+    """ Place value num_valu in cell num_cell,
+    and update all pencil-marks """
+    if not is_valid(sdk_l) : raise RuntimeError("Not a valid sdk in: place()")
+    sdk_l['s'] = set_cell_i(num_cell, num_valu, sdk_l['s'])
+    sdk_l['p'] = pncl_rem(num_cell, num_valu, sdk_l['p'])  # SSS This can be speeded up
+    sdk_l['p'][num_cell] = set()  # and clear all own pencil-marks
     return sdk_l
+
+def un_place(num_cell, sdk_l):
+    """ Un-Place (return to 0) value in cell num_cell,
+    and update all pencil-marks """
+    if not is_valid(sdk_l) : raise RuntimeError("Not a valid sdk in: place()")
+    set_cell(num_cell, 0, sdk_l)  # re-set cell to empty
+    sdk_l = pncl_make(sdk_l)  # re-build pencil marks
+    return sdk_l
+
+def list_empty_fields_id_i(lst_sdk):
+    """ Return a list of id number (positions) for all empty cells """
+    return [n for n in range(len(lst_sdk)) if lst_sdk[n] == 0]
 
 def list_empty_fields_id(sdk_l):
     """ Return a list of id number (positions) for all empty cells """
-    if not valid_sdk(sdk_l) : raise RuntimeError("Not a valid sdk in: list_empty_fields_id()")
-    return [n for n in range(len(sdk_l)) if sdk_l[n] == 0]
+    if not is_valid(sdk_l) : raise RuntimeError("Not a valid sdk in: list_empty_fields_id()")
+    return list_empty_fields_id_i(sdk_l['s'])
 
-def i_make_pcm(lst_s):
+def pncl_make_i(lst_s):
     """ From a SuDoKu (list of int) make PenCilMarks (list of set of int)
     A pencil mark indicates that this value is 'possible' in this cell """
     lst_p = list()
@@ -132,34 +142,69 @@ def i_make_pcm(lst_s):
         lst_p.append(set_n)
     return lst_p
 
-def sudoku_is_solved(sdk_l):
+def pncl_make(sdk_l):
+    """ Re-do, from scratch, all pencilmarks, based on present SuDoKu values
+    Used initially, and after each roll-back. """
+    sdk_l['p'] = pncl_make_i(sdk_l['s'])
+    return sdk_l
+
+def pncl_rem(num_cell, num_valu, lst_pnc_l):
+    """ Remove all pencil-marks, as if num_cell is given num_valu
+    return updated list """
+    for m in range(81):  # update pencil-marks
+        if same_col(m, num_cell) or same_row(m, num_cell) or same_box(m, num_cell):
+            lst_pnc_l[m].discard(num_valu)
+    return lst_pnc_l
+
+def find_min_p_count(sdk_l):
+    lst_empty = list_empty_fields_id(sdk_l)
+    return min([len(sdk_l['p'][n]) for n in lst_empty])
+
+def find_min_p_cell(sdk_l):
+    lst_empty = list_empty_fields_id(sdk_l)
+    num_min_p_count = find_min_p_count(sdk_l)
+    for num_cand in lst_empty:
+        if len(sdk_l['p'][num_cand]) == num_min_p_count:
+            return num_cand
+    return None
+
+def guess(num_cell, sdk_l):
+    if sdk_l['s'][num_cell] == 0:
+        if len(sdk_l['p'][num_cell]) > 0:
+            return next(iter(sdk['p'][num_cell]))  # select a value, without remove
+        else:
+            return None  # Empty cell with no options left, i.e. a dead end...
+    else:
+        return sdk_l['s'][num_cell]  # cell is all ready filled, no need to guess
+
+def is_solved(sdk_l):
     """ Return True if the SuDoKu is full (no 0 left), otherwise False """
-    if not valid_sdk(sdk_l): raise RuntimeError("Not a valid sdk in: sudoku_is_solved()")
+    if not is_valid(sdk_l): raise RuntimeError("Not a valid sdk in: is_solved()")
     lst_sdk = sdk_l['s']
     return not any([n==0 for n in lst_sdk])
 
-def sudoku_is_dead_end(sdk_l):
+def is_dead_end(sdk_l):
     """  Return True if the SuDoKu is in a dead-lock situation, otherwise False
-     A dead situation is if a field is empty (0) but has 9 pencil-marks. """
-    if not valid_sdk(sdk_l): raise RuntimeError("Not a valid sdk in: sudoku_is_dead_end()")
-    lst_empty_fields = list_empty_fields_id(sdk_l['s'])
+     A dead situation is if a field is empty (0) but has no pencil-marks left. """
+    if not is_valid(sdk_l): raise RuntimeError("Not a valid sdk in: is_dead_end()")
+    lst_empty_fields = list_empty_fields_id(sdk_l)
     return any([len(sdk_l['p'][n]) == 9 for n in lst_empty_fields])
 
-def brute(loi_in):
-    """ Brute force - slow solve, all solutions
-    Simply check all possible solutions
-    :param loi_in:
-    :return:
-    """
+# def brute(loi_in):
+#     """ Brute force - slow solve, all solutions
+#     Simply check all possible solutions
+#     :param loi_in:
+#     :return:
+#     """
 
-def q_solve(lst_sdk):
-    """ Solves the SuDoKu
-    Solves the SuDoKu using a minimal algorithm, but using a few tricks that makes it fast - I hope...
-    :param lst_sdk: the input SoDuKo as a standard list of integers, length = 81
-    :return:
-    """
+# def q_solve(lst_sdk):
+#     """ Solves the SuDoKu
+#     Solves the SuDoKu using a minimal algorithm, but using a few tricks that makes it fast - I hope...
+#     :param lst_sdk: the input SoDuKo as a standard list of integers, length = 81
+#     :return:
+#     """
 
-def r_solver(sdk_l, roll_back=False):
+def r_solver(sdk_l):
     """ Recursive solver
     Go for the pencil marks... Go for the field with max pencil marks, i.e. minimum possible openings.
     Call this function (recursively) with the (few) options.
@@ -169,39 +214,41 @@ def r_solver(sdk_l, roll_back=False):
     :param lst_sol: List of solutions, found until this point - can be an empty list
     :return: lst_sol. A, potentially updated, list of solutions - can be an empty list
     """
-    if not valid_sdk(sdk_l) : raise RuntimeError("Not a valid sdk in: r_solver()")
+    if not is_valid(sdk_l) : raise RuntimeError("Not a valid sdk in: r_solver()")
     log.info(f"r_solver(): {len([n for n in sdk_l['s'] if n != 0])}")
+    # if len(list_empty_fields_id(sdk_l)) > 0:
+    #     print(f"{len(list_empty_fields_id(sdk_l))} left")
     # Check if we have a solved SoDuKo
-    if sudoku_is_solved(sdk_l):
-        set_sol.add(''.join(str(i) for i in lst_sdk))  # add as string
-        print("Solution...")
-        return lst_sdk, lst_pnc, set_sol
+    if is_solved(sdk_l):
+        sdk_l['sol'].add(''.join(str(i) for i in lst_sdk))  # add as string
+        print(f"Solution...")
+        return sdk_l, False
     # Check if we have an empty field with no remaining options, i.e. an unsolvable situation
-    if sudoku_is_dead_end(sdk_l):
+    if is_dead_end(sdk_l):
         print("bum")
-        return lst_sdk, lst_pnc, set_sol, True
-    # Pick cell with most pencil-marks, and guess a vacant value
-    lst_empty = list_empty_fields_id(lst_sdk)
-    num_most_pcm = max([len(lst_pnc[n]) for n in lst_empty])
-    num_here = [n for n in lst_empty if len(lst_pnc[n]) == num_most_pcm][0]
-    num_guess = [n for n in range(1, 10) if n not in lst_pnc[num_here]][0]
-    lst_sdk_n, lst_pnc_n, set_sol_n, bol_rb = _r_solver(set_cell(num_here, num_guess, lst_sdk), upd_pncl(num_here, num_guess, lst_pnc), set_sol)
+        return sdk_l, True
+    # Pick cell with minimum pencil-marks, and guess a vacant value
+    num_here = find_min_p_cell(sdk_l)
+    num_guess = guess(num_here, sdk_l)
+    sdk_l = place(num_guess, num_here, sdk_l)
+    sdk_l, bol_rb = r_solver(sdk_l)
     # if roll-back, undo last guess
     if bol_rb:  # We hit a problem, and need to roll-back
-        pass
-    return lst_sdk, lst_pnc, set_sol
+        log.debug(f"roll-back guess:{num_guess}")
+        sdk_l = un_place(num_here, sdk_l)
+    return sdk_l, False
 
 if __name__ == "__main__":
-    lst_sdk = str2loi(".4.8.52...2..4..5.5.......4.9...312.1.6.78..337.9.4.8......67....8359.1..19..76..")
+    #lst_sdk = str2loi(".4.8.52...2..4..5.5.......4.9...312.1.6.78..337.9.4.8......67....8359.1..19..76..")
     #lst_sdk = str2loi("305790081,000008000,010200739,200060890,001000200,008502000,500047003,603005900,009603000")  # 1
-    #lst_sdk = str2loi("000390500,908000603,000000009,000063200,070020006,600010800,000000700,300007000,009100300")  # 2
+    lst_sdk = str2loi("000390500,908000603,000000009,000063200,070020006,600010800,000000700,300007000,009100300")  # 2
     #lst_sdk = str2loi("")  # 3
     #lst_sdk = str2loi("")  # 4
     #lst_sdk = str2loi("")  # 5
-    print("Start")
+    #lst_sdk = str2loi("295743861,431865900,876192543,387459216,612387495,549216738,763524189,928671354,154938600")  # multi solution A
+    #lst_sdk = str2loi("906070403,000400200,070023010,500000100,040208060,003000005,030700050,007005000,405010708")  # multi solution A (many empty=
     sdk = loi2sdk(lst_sdk)
-    print(show_small(sdk))
-    print(sdk)
-    sdk = r_solver(sdk)
-    print("Done...")
+    print(f"Start\n{show_small(sdk)}")
+    sdk, bol = r_solver(sdk)
+    print(f"Done\n{show_small(sdk)}")
 
--- sudoku/SuDoKuS_test.py
+++ sudoku/SuDoKuS_test.py
@@ -39,7 +39,17 @@ class BuildUpFunctions(unittest.TestCase):
         self.assertEqual(sdk['s'], loi_new)
         self.assertEqual(sdk['p'], [{6,7,9},set(),{1,3,7},set(),{1,3,9},set(),set(),{3,6,7,9},{1,6,7,9},{7,8,9},set(),{1,3,7},{1,7},set(),{1},{3,8,9},set(),{1,7,8,9},set(),{3,6,8},{1,3,7},{1,2,7},{1,2,3,9},{1,2},{3,8,9},{3,6,7,9},set(),{4,8},set(),{4,5},{5},{6},set(),set(),set(),{5,6,7},set(),{5},set(),{2,5},set(),set(),{4,5,9},{4,9},set(),set(),set(),{2,5},set(),{1,2,6},set(),{5},set(),{5,6},{2,4},{3,5},{2,3,4,5},{1,2,4},{1,2,8},set(),set(),{3,4,9},{2,5,8,9},{2,4,6,7},{6},set(),set(),set(),set(),{4},set(),{2},{2,4},set(),set(),{2,4},{2,8},set(),set(),{3,4},{2,5,8}])
 
-
+    def test_Guessing(self):
+        loi = SuDoKuS.str2loi(".4.8.52...2..4..5.5.......4.9...312.1.6.78..337.9.4.8......67....8359.1..19..76..")
+        sdk = SuDoKuS.loi2sdk(loi)
+        lst_empty_fields = SuDoKuS.list_empty_fields_id(sdk)
+        self.assertEqual(lst_empty_fields, [0,2,4,7,8,9,11,12,14,15,17,19,20,21,22,23,24,25,27,29,30,31,35,37,39,42,43,47,49,51,53,54,55,56,57,58,61,62,63,64,69,71,72,75,76,79,80])
+        min_p_count = SuDoKuS.find_min_p_count(sdk)
+        self.assertEqual(min_p_count, 1)
+        min_p_cell = SuDoKuS.find_min_p_cell(sdk)
+        self.assertEqual(min_p_cell, 14)
+        guess = SuDoKuS.guess(min_p_cell, sdk)
+        #self.assertEqual(guess, 1)
 
 if __name__ == '__main__':
     unittest.main()
